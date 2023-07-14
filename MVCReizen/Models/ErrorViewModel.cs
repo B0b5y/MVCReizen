44b9399e9b98ae3261ab3f1716b086c70a42e271
B0b5y <112172957+B0b5y@users.noreply.github.com>
2023-07-14 11:36:36 +0200

    Add project files.

---
+++ MVCReizen/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCReizen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
