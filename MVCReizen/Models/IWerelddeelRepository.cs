74d4c3e7fa8fd68cc1c33a0c389ca3b703520dd9
B0b5y <112172957+B0b5y@users.noreply.github.com>
2023-08-16 08:51:25 +0200

    Rep plus Interface

---
+++ MVCReizen/Models/IWerelddeelRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MVCReizen.Models
+{
+    public interface IWerelddeelRepository
+    {
+        IEnumerable<Werelddeel> GetAllWerelddelen();
+        Werelddeel GetWerelddeelById(int id);
+        void AddWerelddeel(Werelddeel werelddeel);
+        void UpdateWerelddeel(Werelddeel werelddeel);
+        void DeleteWerelddeel(int id);
+    }
+}
--- MVCReizen/Repositories/WerelddeelRepository.cs
+++ MVCReizen/Repositories/WerelddeelRepository.cs
@@ -1,11 +1,50 @@
 ﻿using MVCReizen.Models;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace MVCReizen.Repositories;
-
-public class WerelddeelRepository : IWerelddeelRepository
+namespace MVCReizen.Repositories
 {
-    private readonly ReizenContext context:
+    public class WerelddeelRepository : IWerelddeelRepository
+    {
+        private readonly ReizenContext _context;
 
+        public WerelddeelRepository(ReizenContext context)
+        {
+            _context = context;
+        }
 
+        public IEnumerable<Werelddeel> GetAllWerelddelen()
+        {
+            return _context.Werelddelen.ToList();
+        }
+
+        public Werelddeel GetWerelddeelById(int id)
+        {
+            return _context.Werelddelen.Find(id);
+        }
+
+        public void AddWerelddeel(Werelddeel werelddeel)
+        {
+            _context.Werelddelen.Add(werelddeel);
+            _context.SaveChanges();
+        }
+
+        public void UpdateWerelddeel(Werelddeel werelddeel)
+        {
+            _context.Werelddelen.Update(werelddeel);
+            _context.SaveChanges();
+        }
+
+        public void DeleteWerelddeel(int id)
+        {
+            var werelddeel = _context.Werelddelen.Find(id);
+            if (werelddeel != null)
+            {
+                _context.Werelddelen.Remove(werelddeel);
+                _context.SaveChanges();
+            }
+        }
+    }
 }
